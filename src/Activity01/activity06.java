e89ad3092c3abcfa188349d12ed09e53fed1be11
Gehan Liyanage <gehanliyanage@outlook.com>
2024-06-05 09:07:46 +0530

    Second Push

--- src/Activity01/activity06.java
+++ src/Activity01/activity06.java
@@ -6,20 +6,20 @@ public class activity06 {
 
     public static void main(String[] args) {
 
-        // Initialize Scanner for user input
+
         Scanner scanner = new Scanner(System.in);
 
-        // Number of students and subjects
+
         final int NUM_STUDENTS = 5;
         final int NUM_SUBJECTS = 4;
 
-        // 2D array to hold marks of students
+
         int[][] marks = new int[NUM_STUDENTS][NUM_SUBJECTS];
 
-        // Array to hold the names of the subjects
+
         String[] subjects = {"English", "Music", "Geography", "History"};
 
-        // Input marks for each student
+
         for (int i = 0; i < NUM_STUDENTS; i++) {
             System.out.println("Enter marks for student " + (i + 1) + ":");
             for (int j = 0; j < NUM_SUBJECTS; j++) {
@@ -29,7 +29,7 @@ public class activity06 {
             System.out.println();
         }
 
-        // Display the marks for each student
+
         System.out.println("Marks of all students:");
         for (int i = 0; i < NUM_STUDENTS; i++) {
             System.out.print("Student " + (i + 1) + ": ");
@@ -39,7 +39,7 @@ public class activity06 {
             System.out.println();
         }
 
-        // Close the scanner
+
         scanner.close();
     }
 }
